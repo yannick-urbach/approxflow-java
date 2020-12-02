20f10f658faf97d993a77432e0393171d6de521b
Yannick Urbach <yannick.urbach@student.kit.edu>
2020-12-02 11:57:06 +0100

    Fixed AddDummyThrow when invoked repeatedly
    Create instruction sequence every time, because reusing it doesn't seem
    to work

--- src/urbachyannick/approxflow/codetransformation/AddDummyThrow.java
+++ src/urbachyannick/approxflow/codetransformation/AddDummyThrow.java
@@ -12,17 +12,19 @@ import static urbachyannick.approxflow.codetransformation.BytecodeUtil.*;
  * For some reason this doesn't seem to work if externalized into a method.
  */
 public class AddDummyThrow extends Transformation {
-    private static final InsnList dummyThrow = new InsnList() {{
-        add(new TypeInsnNode(Opcodes.NEW, "Ljava/lang/AssertionError;")); // allocate error
-        add(new InsnNode(Opcodes.DUP)); // duplicate reference to feed one into constructor
-        add(new MethodInsnNode( // call constructor
-                Opcodes.INVOKESPECIAL,
-                "Ljava/lang/AssertionError;",
-                "<init>",
-                "()V"
-        ));
-        add(new InsnNode(Opcodes.ATHROW)); // throw error
-    }};
+    private static InsnList dummyThrow() {
+        return new InsnList() {{
+            add(new TypeInsnNode(Opcodes.NEW, "Ljava/lang/AssertionError;")); // allocate error
+            add(new InsnNode(Opcodes.DUP)); // duplicate reference to feed one into constructor
+            add(new MethodInsnNode( // call constructor
+                    Opcodes.INVOKESPECIAL,
+                    "Ljava/lang/AssertionError;",
+                    "<init>",
+                    "()V"
+            ));
+            add(new InsnNode(Opcodes.ATHROW)); // throw error
+        }};
+    }
 
     @Override
     public void apply(ClassNode sourceClass, ClassNode targetClass) throws IOException, InvalidTransformationException {
@@ -35,7 +37,7 @@ public class AddDummyThrow extends Transformation {
 
         for (AbstractInsnNode n = body.getFirst(); n != null; n = n.getNext()) {
             if (n.getOpcode() == Opcodes.RETURN) {
-                method.instructions.insertBefore(n, dummyThrow);
+                method.instructions.insertBefore(n, dummyThrow());
             }
         }
     }
