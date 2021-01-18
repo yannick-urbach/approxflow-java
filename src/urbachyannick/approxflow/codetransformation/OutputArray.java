a58cdb220df17c92a33748f2ae896dad3de3aabf
Yannick Urbach <yannick.urbach@student.kit.edu>
2021-01-18 09:14:58 +0100

    Assertion output array bug workaround
    Check if class init code for assertion status is present; if so, use
    address offset 3.

--- src/urbachyannick/approxflow/codetransformation/OutputArray.java
+++ src/urbachyannick/approxflow/codetransformation/OutputArray.java
@@ -1,7 +1,7 @@
 package urbachyannick.approxflow.codetransformation;
 
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.*;
 import urbachyannick.approxflow.MiscUtil;
 import urbachyannick.approxflow.cnf.*;
 import urbachyannick.approxflow.javasignatures.*;
@@ -14,9 +14,12 @@ import static urbachyannick.approxflow.codetransformation.BytecodeUtil.*;
 public class OutputArray implements Scanner<IntStream> {
     @Override
     public IntStream scan(Stream<ClassNode> sourceClasses, MappedProblem problem) {
+
+        List<ClassNode> classesList = sourceClasses.collect(Collectors.toList());
+        int addressOffset = hasAssertionClassInitCode(classesList.stream()) ? 3 : 0;
         List<VariableMapping> mappings = problem.getVariableTable().getMappings().collect(Collectors.toList());
 
-        return sourceClasses.flatMapToInt(sourceClass ->
+        return classesList.stream().flatMapToInt(sourceClass ->
                 sourceClass.fields.stream().flatMapToInt(field -> {
 
                     if (!hasFlag(field.access, Opcodes.ACC_STATIC))
@@ -49,7 +52,9 @@ public class OutputArray implements Scanner<IntStream> {
                         return IntStream.empty();
                     }
 
-                    long address = MiscUtil.parseAddressFromTrivialLiterals(varLine.get().getMappingValues().map(v -> (TrivialMappingValue) v));
+                    long address = MiscUtil.parseAddressFromTrivialLiterals(
+                            varLine.get().getMappingValues().map(v -> (TrivialMappingValue) v)
+                    ) - addressOffset;
 
                     // all lines of elements of the array with this address, grouped by the element index
                     Map<Long, List<VariableMapping>> elementLines = mappings.stream()
@@ -73,5 +78,36 @@ public class OutputArray implements Scanner<IntStream> {
                     return literals;
                 })
         );
+    }
+
+    public Boolean hasAssertionClassInitCode(Stream<ClassNode> classes) {
+        return classes.anyMatch(class_ -> {
+
+            Optional<MethodNode> classInit = findMethod(class_, class_.name, "<clinit>", "()V");
+
+            return classInit.map(i -> {
+
+                for (AbstractInsnNode insn : i.instructions) {
+
+                    if (insn.getType() != AbstractInsnNode.FIELD_INSN)
+
+                    if (insn.getOpcode() != Opcodes.PUTSTATIC)
+                        continue;
+
+                    FieldInsnNode fieldInsn = (FieldInsnNode) insn;
+
+                    if (!fieldInsn.owner.equals(class_.name))
+                        continue;
+
+                    if (!fieldInsn.name.equals("$assertionsDisabled"))
+                        continue;
+
+                    if (fieldInsn.desc.equals("Z"))
+                        return true;
+                }
+
+                return false;
+            }).orElse(false);
+        });
     }
 }
